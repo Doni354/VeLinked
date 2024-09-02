6122985b950752b2831616586d5448f53c4c6f46
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-09-02 11:17:19 +0700

    Login PWA

---
+++ public/VeLinked-PWA/js/auth.js
@@ -0,0 +1,26 @@
+// Function to handle Google login
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            // Redirect to dashboard after successful login
+            window.location.href = "./dashboard.html";
+        })
+        .catch(console.log);
+}
+
+// Function to handle Google Logut
+function logout() {
+    console.log("Logout button clicked."); // Debugging message
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful, redirect to index.html
+        window.location.href = "./index.html";
+    }).catch(error => {
+        // An error happened
+        console.error("Error while logging out:", error);
+    });
+}
+
+
