b22d8e8da4e192254384326124efd0fdb8ce2adc
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-09-02 14:19:58 +0700

    PWA Login dummy data + Adjust UI

--- public/VeLinked-PWA/js/config.js
+++ public/VeLinked-PWA/js/config.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyB9qUzLViR5uALw9Qf_xzJpd20acoV0FEs",
     authDomain: "velinked-web.firebaseapp.com",
@@ -18,7 +13,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+initializeApp(firebaseConfig);
 
--- public/js/a-pp.js
+++ public/js/a-pp.js
@@ -1,15 +1,35 @@
-// Fungsi untuk menampilkan foto profil
+// Fungsi untuk login menggunakan Google
+function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            // Redirect ke dashboard setelah login berhasil
+            window.location.href = "./dashboard.html";
+        })
+        .catch(console.log);
+}
+
+// Fungsi untuk logout
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Berhasil logout, redirect ke index.html
+        window.location.href = "./index.html";
+    }).catch((error) => {
+        console.error("Error while logging out:", error);
+    });
+}
+
 function displayUserProfilePic(user) {
     const profilePicDiv = document.getElementById("profile-picture");
     const userNameDiv = document.getElementById("user-name");
-    
+
     // Cek apakah pengguna sudah login
     if (user) {
         // Buat elemen gambar untuk foto profil pengguna
         const profilePic = document.createElement("img");
         profilePic.src = user.photoURL;
         profilePic.classList.add("profile-pic"); // Tambahkan kelas khusus untuk styling
-        
+
         // Tambahkan event listener untuk memeriksa apakah gambar berhasil dimuat
         profilePic.addEventListener("error", function() {
             // Jika gambar gagal dimuat, atur sumbernya ke gambar profil default
@@ -29,18 +49,16 @@ function displayUserProfilePic(user) {
 
 // Event listener untuk memastikan kode dijalankan setelah konten dimuat
 document.addEventListener("DOMContentLoaded", event => {
-    // Check if user is logged in
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             // If user is logged in, display user information
             displayUserProfilePic(user);
 
             const welcomeMessage = document.getElementById("welcome-message");
-        welcomeMessage.textContent += user.displayName;
+            welcomeMessage.textContent += user.displayName;
         } else {
             // If user is not logged in, redirect to login page
             window.location.href = "login.html";
         }
     });
 });
-
